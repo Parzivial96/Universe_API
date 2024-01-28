84163092e91bfbb99e5fdb1ff2aff0f5d349bf23
Parzivial96 <yogesh.pdy4@gmail.com>
2024-01-28 15:16:53 +0530

    Edited default value in game model

--- api/models.py
+++ api/models.py
@@ -5,5 +5,5 @@ class Game(models.Model):
     title = models.CharField(primary_key=True,max_length=100)
     price = models.IntegerField(default=0)
     banner = models.ImageField(upload_to='api/static/banners')
-    generes = models.JSONField(default=[])
-    platforms = models.JSONField(default=[])
+    generes = models.JSONField(default=list)
+    platforms = models.JSONField(default=list)
